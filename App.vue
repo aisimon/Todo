67b98ddf61f8789888c25369fcf34edd4a33334b
Simon Li <simon.li@shiptheory.com>
2025-07-20 19:34:45 +0100

    fix: update Todo app UI by enhancing button layout and adding Font Awesome icons

--- App.vue
+++ App.vue
@@ -9,15 +9,21 @@
         </button>
       </div>
       <div class="flex mb-3">
-        <input type="text" class="flex-grow p-2 border rounded-l-md text-gray-800 dark:text-white bg-white dark:bg-gray-700"
+        <button class="std p-2 bg-blue-500 text-white rounded-l-md flex items-center justify-center" @click="addTodo()">
+          <i class="fas fa-plus"></i>
+        </button>
+        <input type="text" class="flex-grow p-2 border rounded-r-md text-gray-800 dark:text-white bg-white dark:bg-gray-700"
           v-model="newTodo" @keyup.enter="addTodo" placeholder="Add a new todo">
-        <button class="std p-2 bg-blue-500 text-white rounded-r-md" @click="addTodo()">Add</button>
       </div>
       <ul class="space-y-2">
         <li class="flex justify-between items-center bg-gray-100 rounded-md dark:bg-gray-700 dark:text-white"
           v-for="(todo, index) in todos" :key="index">
-          <span class="p-2">{{ todo }}</span>
-          <button class="std p-2 bg-red-600 text-white rounded-md" @click="deleteTodo(index)"> Done </button>
+          <div class="flex items-center">
+            <button class="std p-2 bg-red-600 text-white rounded-l-md" @click="deleteTodo(index)">
+              <i class="fas fa-circle"></i>
+            </button>
+            <span class="p-2">{{ todo }}</span>
+          </div>
         </li>
       </ul>
     </div>
@@ -66,9 +72,10 @@ export default {
 <style>
 /* Tailwind's dark mode styles are now fully handled by the `dark:` prefix. */
 @import url('https://fonts.googleapis.com/icon?family=Material+Icons');
+@import url('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css');
 
 button.std {
-  width: 65px;
+  width: 60px;
   height: rem(3.5); /* Adjust height to match button size */
 }
 </style>
